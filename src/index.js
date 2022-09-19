8ab70250df572bfa8feee0eda3c496faeab5d857
Akshay Raichur <lordakshay99@gmail.com>
2022-09-19 13:49:50 +0530

    chore(README): updated README with started code

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const startedCode = () => {
+  return 'this package is still in development, connect with akshay raichur (https://github.com/akshayraichur) for more ideas.';
+};
+
+module.exports = startedCode;
