1b483e0beeccdac96c0b52c85c59876e4e044625
Akshay Raichur <lordakshay99@gmail.com>
2022-09-20 10:40:01 +0530

    chore: checked out from dev branch to work on different cases

---
+++ bin/Constants.js
@@ -0,0 +1,16 @@
+export const OS_PLATFORMS = {
+  MACOS: 'darwin',
+  LINUX: 'linux',
+  WINDOWS: 'win32',
+  ANDROID: 'android',
+};
+
+export const OPTIONS = {
+  font: ['inter', 'poppins'],
+  type: ['modular', 'basic'],
+};
+
+export const COMMANDS_TO_EXECUTE = {
+  CLONE: ``, //subject to change because of fonts and module and stuff
+  MOVE: `cd `,
+};
--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
-const { execSync } = require('child_process');
+import { execSync } from 'child_process';
+import chalk from 'chalk';
+import path from 'path';
+import { program } from 'commander';
+
+const log = console.log;
+const blue = chalk.blue;
+const red = chalk.red;
+const green = chalk.green;
 
 const runCommand = (command) => {
   try {
@@ -11,13 +19,80 @@ const runCommand = (command) => {
   return true;
 };
 
+program.version('1.1.0');
+
+program.option(
+  '-f, --font <type>',
+  'specify the font that you want to install from the given fonts',
+  'Poppins'
+);
+
+program.option(
+  '-t, --type <type>',
+  'specify whether you want modular js boilerplate code or not',
+  'basic'
+);
+
+program.parse(process.argv);
+const options = program.opts();
+console.log('options :', options);
+
 const dirName = process.argv[2];
+
+if (dirName === undefined) {
+  log(red('You have not specified the project name.'));
+  process.exit(-1);
+}
+
+const root = path.resolve(dirName);
+
+log(blue(`Creating a javascript app in ${green(root)}`));
+log(process.argv);
+log(process.platform);
+
 const gitCheckout = `git clone --dept 1 https://github.com/akshayraichur/js-app-template.git ${dirName}`;
 
 console.log(`Cloning the repo with name ${dirName}`);
 
 const checkedOut = runCommand(gitCheckout);
 if (!checkedOut) process.exit(-1);
+
+const moveToAppDir = runCommand(`cd ${dirName}`);
+if (!moveToAppDir) process.exit(-1);
+
+// remove git history
+const removeGitCommand = `rm -rf .git`;
+const removeGit = runCommand(removeGitCommand);
+
+if (!removeGit)
+  console.log(
+    red(
+      'we could not remove git from this repo to re-initialize, please do it manually'
+    )
+  );
+
+// Initialize git
+const gitInitialize = `git init`;
+const checkGitInitialization = runCommand(gitInitialize);
+
+if (!checkGitInitialization)
+  console.log(red('we could not initialize git, please do it manually'));
+else {
+  console.log(green('git iitialized'));
+}
+
+// change branch name to main
+const changeBranchName = `git branch -m main`;
+const checkChangeBranch = runCommand(changeBranchName);
+if (!checkChangeBranch)
+  console.log(
+    red(
+      'we could not change git branch to main from master, please do it manually'
+    )
+  );
+else {
+  console.log(green('git branch changed from master to main'));
+}
 
 console.log(
   `Congratulations! You are ready. Follow the commands to start the project. `
